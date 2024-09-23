0cd89a0db59b88b38e0fedda3c0cfe1954cda381
scherenhaenden <scherenhaenden@gmail.com>
2024-09-23 15:12:27 +0200

    Add initial implementation of lib-magnetar-quill library with core components and services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LibMagnetarQuillComponent} from "lib-magnetar-quill";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LibMagnetarQuillComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
